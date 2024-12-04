b58bdabcc3a16fb6e5d180443ca22e475feac79e
ThatBlackFox <108296426+ThatBlackFox@users.noreply.github.com>
2024-12-04 20:51:16 +0530

    Hot Fix

--- data_handler.py
+++ data_handler.py
@@ -7,7 +7,7 @@ def get_data_frame(path:str, until:datetime=None):
     df = pd.read_csv(path)
     df['Date'] = pd.to_datetime(df['Date'],format=r"%d-%m-%y")
     df = df.fillna(-1)
-    if until:
+    if until!=None:
         df = df[df['Date']<=until]
     return df
 
@@ -18,9 +18,23 @@ def get_all_data(until:datetime=None):
         crop_name = file.split('_price_')[1]
         crop_name = crop_name.split('-upto')[0]
         crop_name = crop_name.split('_upto')[0]
-        df = get_data_frame(file)
+        df = get_data_frame(file,until)
         dataframes[crop_name] = df.to_dict()
     return dataframes
+
+def get_crop_data(crop:str,until:datetime=None):
+    dataframes = {}
+    for file in pathlib.Path('data/').iterdir():
+        file = str(file)
+        crop_name = file.split('_price_')[1]
+        crop_name = crop_name.split('-upto')[0]
+        crop_name = crop_name.split('_upto')[0]
+        dataframes[crop_name] = file
+    
+    if crop in dataframes:
+        return {crop:get_data_frame(dataframes[crop],until).to_dict()}
+    
+    return {}
 
 def get_names():
     crops = []
@@ -31,19 +45,3 @@ def get_names():
         crop_name = crop_name.split('_upto')[0]
         crops.append(crop_name)
     return crops
-
-def get_crop_data(crop:str,until:datetime=None):
-    dataframes = {}
-    for file in pathlib.Path('data/').iterdir():
-        file = str(file)
-        crop_name = file.split('_price_')[1]
-        crop_name = crop_name.split('-upto')[0]
-        crop_name = crop_name.split('_upto')[0]
-        dataframes[crop_name] = None
-    
-    if crop in dataframes:
-        return get_data_frame(crop)
-    
-    return {}
-
-
