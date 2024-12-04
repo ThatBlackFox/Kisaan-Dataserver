711c58d252fc8fa1ce6446410ebbb2cc81d5876b
Amogha Y A <108296426+ThatBlackFox@users.noreply.github.com>
2024-12-04 17:37:47 +0530

    Add until parameter

--- data_handler.py
+++ data_handler.py
@@ -1,8 +1,49 @@
 import pandas as pd
 import os
 import pathlib
-files = []
-for file in pathlib.Path('data/').iterdir():
-    files.append(file.joinpath(file))
+from datetime import datetime
 
-print(files)
+def get_data_frame(path:str, until:datetime=None):
+    df = pd.read_csv(path)
+    df['Date'] = pd.to_datetime(df['Date'],format=r"%d-%m-%y")
+    df = df.fillna(-1)
+    if until:
+        df = df[df['Date']<=until]
+    return df
+
+def get_all_data(until:datetime=None):
+    dataframes = {}
+    for file in pathlib.Path('data/').iterdir():
+        file = str(file)
+        crop_name = file.split('_price_')[1]
+        crop_name = crop_name.split('-upto')[0]
+        crop_name = crop_name.split('_upto')[0]
+        df = get_data_frame(file)
+        dataframes[crop_name] = df.to_dict()
+    return dataframes
+
+def get_names():
+    crops = []
+    for file in pathlib.Path('data/').iterdir():
+        file = str(file)
+        crop_name = file.split('_price_')[1]
+        crop_name = crop_name.split('-upto')[0]
+        crop_name = crop_name.split('_upto')[0]
+        crops.append(crop_name)
+    return crops
+
+def get_crop_data(crop:str,until:datetime=None):
+    dataframes = {}
+    for file in pathlib.Path('data/').iterdir():
+        file = str(file)
+        crop_name = file.split('_price_')[1]
+        crop_name = crop_name.split('-upto')[0]
+        crop_name = crop_name.split('_upto')[0]
+        dataframes[crop_name] = None
+    
+    if crop in dataframes:
+        return get_data_frame(crop)
+    
+    return {}
+
+
--- proxy.py
+++ proxy.py
@@ -1,22 +1,41 @@
 from fastapi import FastAPI
 from models import *
+from data_handler import *
 from unqlite import UnQLite as unq
+from datetime import datetime
 
-db = unq('data.db')
+db = {}
+db['date'] = datetime.today()
 app = FastAPI()
 
-@app.get("/wheat")
-async def wheat():
-    return {"message":"Hello World"}
+@app.get("/crop/{crop}")
+async def get_crop(crop):
+    frames = get_crop_data(crop,until=db['date'])
+    if crop in frames:
+        return {"message":"Historic data", "data":frames[crop]}
+    else:
+        frames = get_all_data(until=db['date'])
+        return {"message":"Crop not found, only the attached crops are available", "data":list(frames.keys())}
+
+@app.get("/crop_names")
+async def get_crop_names():
+    frames = get_all_data(until=db['date'])
+    return {"message":"Attached crops are available", "data":list(frames.keys())}
+
+@app.get("/all_crops")
+async def get_all_crops():
+    frames = get_all_data(until=db['date'])
+    return {"message":"Historic data", "data":frames}
 
 @app.post("/set_date")
-async def set_date(date_model: DateModel):
-    received_date = date_model.date
+async def set_date(date: DateModel):
+    received_date = date.date
     db['date'] = received_date
-    return {"message": "Date received successfully", "date": received_date}
+    return {"message": "Date received successfully", "date": received_date, "debug":str(type((db['date'])))}
 
 @app.get('/get_date')
 async def get_date():
     return {"message":"Date is currently set to the same as the field of date", "date":db['date']}
+
 
 
